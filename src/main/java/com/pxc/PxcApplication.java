9eef1b96b6af42b917cdd87b902595dfd5f36edc
pxc1994 <allenpeng18@gmail.com>
2019-01-20 21:33:12 +0800

    Initial commit

---
+++ src/main/java/com/pxc/PxcApplication.java
@@ -0,0 +1,14 @@
+package com.pxc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PxcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PxcApplication.class, args);
+    }
+
+}
+
