1b8da188d27695cd571cb13f9553d1da061f502c
pxc1994 <allenpeng18@gmail.com>
2019-03-17 18:52:46 +0800

    modify

--- src/main/java/com/demo/DemoApplication.java
+++ src/main/java/com/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package com.pxc;
+package com.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PxcApplication {
+public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PxcApplication.class, args);
+        SpringApplication.run(DemoApplication.class, args);
     }
 
 }
--- src/test/java/com/demo/PxcApplicationTests.java
+++ src/test/java/com/demo/PxcApplicationTests.java
@@ -1,4 +1,4 @@
-package com.pxc;
+package com.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
